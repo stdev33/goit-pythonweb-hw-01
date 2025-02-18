7e0b87df3f46fd70686be034709485d6ebac6d01
Serhii Tarasenko <stdev33@gmail.com>
2025-02-18 16:41:48 +0200

    Task 1. Fabric pattern

--- task_1.py
+++ task_1.py
@@ -1,5 +1,68 @@
+from abc import ABC, abstractmethod
+import logging
 
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class Vehicle(ABC):
+    def __init__(self, make: str, model: str, region_spec: str):
+        self.make = make
+        self.model = model
+        self.region_spec = region_spec
+
+    @abstractmethod
+    def start_engine(self) -> None:
+        pass
+
+
+class Car(Vehicle):
+    def start_engine(self) -> None:
+        logger.info(
+            f"{self.make} {self.model} ({self.region_spec} Spec): Двигун запущено"
+        )
+
+
+class Motorcycle(Vehicle):
+    def start_engine(self) -> None:
+        logger.info(
+            f"{self.make} {self.model} ({self.region_spec} Spec): Мотор заведено"
+        )
+
+
+class VehicleFactory(ABC):
+    @abstractmethod
+    def create_car(self, make: str, model: str) -> Vehicle:
+        pass
+
+    @abstractmethod
+    def create_motorcycle(self, make: str, model: str) -> Vehicle:
+        pass
+
+
+class USVehicleFactory(VehicleFactory):
+    def create_car(self, make: str, model: str) -> Vehicle:
+        return Car(make, model, "US")
+
+    def create_motorcycle(self, make: str, model: str) -> Vehicle:
+        return Motorcycle(make, model, "US")
+
+
+class EUVehicleFactory(VehicleFactory):
+    def create_car(self, make: str, model: str) -> Vehicle:
+        return Car(make, model, "EU")
+
+    def create_motorcycle(self, make: str, model: str) -> Vehicle:
+        return Motorcycle(make, model, "EU")
 
 
 if __name__ == "__main__":
-    # Test
+    us_factory = USVehicleFactory()
+    eu_factory = EUVehicleFactory()
+
+    vehicle1 = us_factory.create_car("Toyota", "Corolla")
+    vehicle2 = eu_factory.create_motorcycle("Harley-Davidson", "Sportster")
+
+    vehicle1.start_engine()
+    vehicle2.start_engine()
